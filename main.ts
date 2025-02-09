c73f049f7f1c9c1c0ea68a3ee63a0adda7a5c3d7
darienlabs <de.darienewing@gmail.com>
2025-02-09 15:04:02 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 namespace SpriteKind {
-    export const Text = SpriteKind.create()
+    export const Friend = SpriteKind.create()
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, sprites.castle.tilePath5)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     player1,
@@ -81,6 +86,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (nearsign == true) {
         tiles.setCurrentTilemap(tilemap`level2`)
         tiles.placeOnTile(player1, tiles.getTileLocation(0, 0))
+    } else if (player1.overlapsWith(villager1)) {
+        choice = game.ask("would you like a  Quest?")
+    } else {
+    	
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
@@ -300,6 +309,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`sign`, function (sprite, location) {
     nearsign = true
+    game.showLongText("road (A)", DialogLayout.Bottom)
 })
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, player1)
@@ -398,34 +408,28 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, sprites.castle.tileGrass3)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.InBackground)
+})
 info.onLifeZero(function () {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.dissolve)
 })
+let choice = false
+let coiny = 0
+let coinx = 0
 let player1: Sprite = null
 let nearsign = false
-let signtext = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Text)
+let villager1: Sprite = null
+let signtext = textsprite.create("press A")
+villager1 = sprites.create(assets.image`myImage0`, SpriteKind.Friend)
 signtext.setPosition(0, 0)
+villager1.setPosition(123, 10)
 nearsign = false
 info.setLife(100)
-music.play(music.createSong(hex`0078000408060305001c000f0a006400f4010a0000040000000000000000000000000000000002930104000800040a0d121e08000c0001250c001000040d121e2914001800040a1d242918001c0002121e1c002000030d252a20002400041e20222924002800040c0f222728002c0003121e222c003000040a20252930003400040d1d202434003800080a0d11161e24272938003c00030d20293c00400002121e40004400030a242744004800040f141d2548004c000224294c00500007060c121e252a2c50005400030f2229540058000508121e242a58005c00040f1e24295c006000030a222960006400041e20252964006800040a0c111668006c0004081e272a6c00700004080d142470007400030d222a74007800040616292a78007c00050c0f1422297c00800005060d1e252c80008400040d14202a840088000308202788008c00050c141e242c8c00900003080f2790009400050c1e22242a9400980002162598009c00060a0f1e22272c9c00a00007080c1214181d27a000a400012aa400a800060d14191d2022a800ac00030a0f29ac00b000030a181eb000b400050a0f182427b400b800020d20b800bc00021627bc00c00002081206001c00010a006400f401640000040000000000000000000000000000000002d00008000c00050d0f1622290c00100002052710001400011614001800030c222a18001c00020c181c00200002202c20002400020c2724002800011428002c000308122a2c003000011e30003400010c3400380002222738003c0002112a40004400030a121848004c00020d224c00500001145000540001085400580001275c006000020c2764006800020f246c007000012774007800030c18207c008000012084008800030a0d248c009000011494009800020f259c00a000021127a800ac000112b000b400020d24bc00c000030d1d2908001c000e050046006603320000040a002d0000006400140001320002010002540058005c0001245c006000010d60006400012268006c00010f6c007000012a74007800021d257c008000010f80008400012488008c00011490009400020f149400980002252c9c00a0000129bc00c000040f16252c`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(hex`0078000408060500001c00010a006400f40164000004000000000000000000000000000500000418003c004000012c40004400012c44004800012c48004c00012c05001c000f0a006400f4010a0000040000000000000000000000000000000002930104000800040a0d121e08000c0001250c001000040d121e2914001800040a1d242918001c0002121e1c002000030d252a20002400041e20222924002800040c0f222728002c0003121e222c003000040a20252930003400040d1d202434003800080a0d11161e24272938003c00030d20293c00400002121e40004400030a242744004800040f141d2548004c000224294c00500007060c121e252a2c50005400030f2229540058000508121e242a58005c00040f1e24295c006000030a222960006400041e20252964006800040a0c111668006c0004081e272a6c00700004080d142470007400030d222a74007800040616292a78007c00050c0f1422297c00800005060d1e252c80008400040d14202a840088000308202788008c00050c141e242c8c00900003080f2790009400050c1e22242a9400980002162598009c00060a0f1e22272c9c00a00007080c1214181d27a000a400012aa400a800060d14191d2022a800ac00030a0f29ac00b000030a181eb000b400050a0f182427b400b800020d20b800bc00021627bc00c00002081206001c00010a006400f401640000040000000000000000000000000000000002d00008000c00050d0f1622290c00100002052710001400011614001800030c222a18001c00020c181c00200002202c20002400020c2724002800011428002c000308122a2c003000011e30003400010c3400380002222738003c0002112a40004400030a121848004c00020d224c00500001145000540001085400580001275c006000020c2764006800020f246c007000012774007800030c18207c008000012084008800030a0d248c009000011494009800020f259c00a000021127a800ac000112b000b400020d24bc00c000030d1d2907001c00020a006400f401640000040000000000000000000000000000000003250000000400012704000800012a0c00100001241000140002112c1c002000011420002400011208001c000e050046006603320000040a002d0000006400140001320002010002540058005c0001245c006000010d60006400012268006c00010f6c007000012a74007800021d257c008000010f80008400012488008c00011490009400020f149400980002252c9c00a0000129bc00c000040f16252c`), music.PlaybackMode.LoopingInBackground)
 player1 = sprites.create(assets.image`Zayne`, SpriteKind.Player)
 controller.moveSprite(player1, 52, 52)
 let HP_bar = statusbars.create(20, 4, StatusBarKind.Health)
@@ -433,31 +437,29 @@ let player_energy = statusbars.create(scene.screenWidth() - 12, 4, StatusBarKind
 HP_bar.attachToSprite(player1, 3, 0)
 player_energy.setLabel("EN", 15)
 player_energy.positionDirection(CollisionDirection.Bottom)
+HP_bar.setColor(2, 15, 0)
 tiles.setCurrentTilemap(tilemap`level`)
 scene.setBackgroundColor(7)
-scene.setBackgroundImage(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `)
 scene.cameraFollowSprite(player1)
 HP_bar.max = 100
+let randcoins = 0
+while (10 < randcoins) {
+    coinx = randint(0, 31)
+    coiny = randint(0, 31)
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(coinx, coiny), sprites.castle.tileGrass1) || tiles.tileAtLocationEquals(tiles.getTileLocation(coinx, coiny), sprites.castle.tileGrass3)) {
+        tiles.setTileAt(tiles.getTileLocation(coinx, coiny), assets.tile`myTile2`)
+        randcoins += 1
+    }
+}
+game.splash("press a to interact")
 game.onUpdate(function () {
     if (!(tiles.tileAtLocationEquals(tiles.getTileLocation(Math.floor(player1.y), Math.floor(player1.x)), assets.tile`sign`))) {
         nearsign = false
+    } else {
+        nearsign = true
+    }
+    if (player1.overlapsWith(villager1)) {
+    	
     }
 })
 forever(function () {
